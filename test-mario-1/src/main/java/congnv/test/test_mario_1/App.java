e6c4a947af198477b237ad0429b12d94d8bc4082
Ngô Văn Công <nvcongnv@gmail.com>
2018-04-12 10:02:05 +0700

    init

---
+++ test-mario-1/src/main/java/congnv/test/test_mario_1/App.java
@@ -0,0 +1,13 @@
+package congnv.test.test_mario_1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
